fd66a79d6952109d985f2c9d316a7f220f4ddfad
Wasim Hyder <wasim@creativefaze.com>
2024-01-19 19:17:45 +0500

    Added dynamic options to navigation layout

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style='dark' />
+      <StatusBar style='light' />
       <NavigationContainer>
         <Stack.Navigator screenOptions={{
           headerStyle: { backgroundColor: '#351401' },
@@ -28,6 +28,12 @@ export default function App() {
           <Stack.Screen
             name='MealsOverview'
             component={MealsOverviewScreen}
+          // options={({ route, navigation }) => {
+          //   const catId = route.params.categoryId;
+          //   return {
+          //     title: catId
+          //   };
+          // }}
           />
         </Stack.Navigator>
       </NavigationContainer>
