e46d97a9a1b9de7c6964f0a6075621781d653345
Wasim Hyder <wasim@creativefaze.com>
2024-01-21 22:43:40 +0500

    Added icon in the header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,38 @@
-import { View, Image, Text, StyleSheet, ScrollView } from 'react-native';
+import { useLayoutEffect } from 'react';
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Button
+} from 'react-native';
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
+import IconButton from '../components/IconButton';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log('Pressed');
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <IconButton
+          icon='star'
+          color='white'
+          onPress={headerButtonPressHandler}
+        />
+      }
+    });
+  }, [navigation, headerButtonPressHandler]);
+
   return <ScrollView style={styles.rootContainer}>
     <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
     <Text style={styles.title}>{selectedMeal.title}</Text>
