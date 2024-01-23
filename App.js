04ecd723e670d2e2ff6377b7c500a877ae5bc14e
Wasim Hyder <wasim@creativefaze.com>
2024-01-23 22:28:33 +0500

    Displayed favorite meals screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from './screens/CategoriesScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
@@ -14,10 +15,39 @@ const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
-  return <Drawer.Navigator>
-    <Drawer.Screen name='Categories' component={CategoriesScreen} />
-    <Drawer.Screen name='Favorites' component={FavoritesScreen} />
-  </Drawer.Navigator>
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' },
+        drawerContentStyle: { backgroundColor: '#351401' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
 }
 
 export default function App() {
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,29 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import MealsList from "../components/MealsList/MealsList";
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const favoriteMeals = [];
+  if (favoriteMeals.length === 0) {
+    return <View style={styles.rootContainer}>
+      <Text style={styles.text}>You have no favorite meals yet.</Text>
+    </View>
+  }
+
+  return <MealsList items={[]} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
